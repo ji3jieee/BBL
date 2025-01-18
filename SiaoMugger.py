b8b013b2316d603ba5e17d48501d5fd92e83401b
ji3jieee <e1155980@u.nus.edu>
2025-01-18 13:30:59 +0800

    Update SiaoMugger.py
    Setup to telegram

---
+++ SiaoMugger.py
@@ -0,0 +1,6 @@
+from typing import Final
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+
+TOKEN: Final = '7870019893:AAG4LYwPcWWdPystb2Ic5vHtyuej9kqtW_Y'
+BOT_USERNAME: Final = '@SiaoMuggerBot'
